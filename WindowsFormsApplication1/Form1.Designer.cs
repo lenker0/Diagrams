9086f6f4aa8e8871a27d9227390e1a851e5add44
Michael <43700151+lenker0@users.noreply.github.com>
2019-05-17 22:33:02 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(760, 300);
+            this.panel1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(329, 318);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(222, 131);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.DoubleClick += new System.EventHandler(this.richTextBox1_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OldLace;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(117, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 131);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OPEN";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Tan;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.BorderSize = 3;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(223, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 131);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "SAVE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(12, 372);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 24);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Linear";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(12, 402);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(84, 24);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Circular";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(8, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "TYPE:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.RosyBrown;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(557, 318);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 131);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "SET";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(659, 372);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(659, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Random:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.Enabled = false;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button4.FlatAppearance.BorderSize = 3;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(648, 414);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 35);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "STOP";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Timers;
+
+
+namespace WindowsFormsApplication1
+{
+    struct Diagram
+    {
+        public float value;
+        public Color color;
+        public Diagram(string Str)
+        {
+            string[] str = Str.Split(' ');
+            int[] rgb = new int[3] { 255, 255, 255 };
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] != "")
+                    rgb[i - 1] = Int32.Parse(str[i]);
+            }
+            this.value = float.Parse(str[0]);
+            this.color = Color.FromArgb(rgb[0], rgb[1], rgb[2]);
+        }
+        public Diagram(float value, int r, int g, int b)
+        {
+            this.value = value;
+            this.color = Color.FromArgb(r, g, b);
+        }
+    }
+    public partial class Form1 : Form
+    {
+        List<Diagram> diag = new List<Diagram>();
+        float k = 1;
+        float sum = 0;
+        bool type = true;
+        float mx, mn;
+        int tick = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+        private void FromBoxToList()
+        {
+            List<Diagram> voiddiag = new List<Diagram>();
+            diag = voiddiag;
+            foreach (string Str in richTextBox1.Lines)
+            {
+                if (Str == "") continue;
+                Diagram tmp = new Diagram(Str);
+                diag.Add(tmp);
+            }
+        }
+        private void FromListToBox()
+        {
+            richTextBox1.Clear();
+            foreach (Diagram elem in diag)
+            {
+                richTextBox1.AppendText(elem.value.ToString() + " " + elem.color.R.ToString() + " " + elem.color.G.ToString() + " " + elem.color.B.ToString()+ "\n");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            clear();
+            string path = openFileDialog1.FileName;
+            using (StreamReader sr = File.OpenText(path))
+            {
+                richTextBox1.AppendText(sr.ReadToEnd());
+            }
+            FromBoxToList();
+            paint();
+        }
+        private void paint()
+        {
+            analyze();
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+            if (diag.Count == 0) return;
+            if (type)
+            {
+                rectangle();
+            }
+            else
+            {
+                circle();
+            }
+        }
+        private void rectangle()
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.TranslateTransform(30, 150);
+            float width=700/ diag.Count;
+            float probel = width * 0.1f;
+            width = width-2*probel;
+            float last = probel;
+            foreach (Diagram elem in diag)
+            {
+                SolidBrush Brush = new SolidBrush(elem.color);
+                if (elem.value > 0)
+                {
+                    g.FillRectangle(Brush, last, -elem.value * k, width, elem.value * k);
+                    g.DrawRectangle(new Pen(Color.Black, 1), last, -elem.value * k, width, elem.value * k);
+                }
+                else
+                {
+                    g.FillRectangle(Brush, last, 0, width, -elem.value * k);
+                    g.DrawRectangle(new Pen(Color.Black, 1), last, 0, width, -elem.value * k);
+                }
+                Brush.Color = Color.Black;
+                Font font = new Font("Century Gothic", 10);
+                int extra = elem.value > 0 ? 15 : 0;
+                g.DrawString(elem.value.ToString(), font, Brush, last, -elem.value * k-extra); 
+                last += 2 * probel + width;
+                font.Dispose(); Brush.Dispose();
+            }
+            g.DrawLine(new Pen(Color.Black, 2.0f), 0, 120, 0, -120);
+            g.DrawLine(new Pen(Color.Black, 2.0f), 0, 0, 700, 0);
+        }
+        private void circle()
+        {
+            Graphics g = panel1.CreateGraphics();
+            float oneagnle = (float)360/sum;
+            float startangle=0f;
+            float height = 15f ;
+            float probel = (float)panel1.Height / (diag.Count+1) - height;
+            float last = probel+height/2;
+            foreach (Diagram elem in diag)
+            {
+                g.FillPie(new SolidBrush(elem.color), 100, 25, 250, 250, startangle, (float)Math.Abs(elem.value * oneagnle));
+                g.DrawPie(new Pen(Color.Black, 1.0f), 100, 25, 250, 250, startangle, (float)Math.Abs(elem.value * oneagnle));
+                startangle += (float)Math.Abs(elem.value * oneagnle);
+                SolidBrush Brush = new SolidBrush(elem.color);
+                g.FillRectangle(Brush, 500, last, height,height);
+                g.DrawRectangle(new Pen(Color.Black, 1), 500, last, height, height);
+                Font font = new Font("Century Gothic", 10);
+                Brush.Color = Color.Black;
+                g.DrawString(elem.value.ToString(), font, Brush, 520, last);
+                font.Dispose(); Brush.Dispose();
+                last += probel + height;
+            }
+        }
+        private void clear()
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+            richTextBox1.Clear();
+            List<Diagram> voiddiag = new List<Diagram>();
+            diag = voiddiag;
+            k = 1; sum = 0;
+        }
+        private void analyze()
+        {
+            sum = 0;
+            if (diag.Count > 0)
+            {
+                mn = diag[0].value;
+                sum += Math.Abs(mn);
+                mx = mn;
+            }
+            for (int i = 1; i < diag.Count; i++)
+            {
+                float elem = diag[i].value;
+                sum += Math.Abs(elem);
+                if (elem > mx) mx = elem;
+                if (elem < mn) mn = elem;
+            }
+            double maxheight = Math.Max(Math.Abs(mx),Math.Abs(mn));
+            k = (float)(120 / maxheight) ;
+        }
+        private void richTextBox1_DoubleClick(object sender, EventArgs e)
+        {
+            clear();
+            generate();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            paint();
+            FromListToBox();
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string path = saveFileDialog1.FileName;
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                for (int i=0;i<richTextBox1.Lines.Length;i++)
+                {
+                    sw.WriteLine(richTextBox1.Lines[i]);
+                }
+            }
+            MessageBox.Show("Data saved");
+        }
+        private void generate()
+        {
+            Random n = new Random();
+            int maxn = n.Next(1, 20);
+            clear();
+            for (int i = 0; i < maxn; i++)
+            {
+                float val = (float)n.Next(-1000, 1000);
+                if (!type) val = (float)n.Next(0, 1000);
+                Diagram tmp = new Diagram(val, n.Next(1, 255), n.Next(1, 255), n.Next(1, 255));
+                diag.Add(tmp);
+            }
+            FromListToBox();
+            paint();
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            type = true;
+            paint();
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            type = false;
+            paint();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FromBoxToList();
+            paint();
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tick % trackBar1.Value == 0)
+            {
+                generate();
+            }
+            tick++;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button4.Enabled = false;
+            trackBar1.Value = 0;
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            tick = 0;
+            timer1.Stop();
+            if (trackBar1.Value != 0)
+            {
+                button4.Enabled = true;
+                timer1.Start();
+            }
+            else
+                button4.Enabled = false;
+        }
+    }
+}
